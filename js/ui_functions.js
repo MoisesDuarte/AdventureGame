910d7c2c139666630827589e59d2080e9682fdc5
MoisesDuarte <moisesduartejunior@gmail.com>
2020-02-21 14:39:16 -0300

    Implementando transição de cena e limpeza de tela

--- js/ui_functions.js
+++ js/ui_functions.js
@@ -1,6 +1,7 @@
-// FUNÇÃO PARA CONSTRUÇÃO DE CENA DE JOGO
+// FUNÇÕES PARA CONSTRUÇÃO DE CENA DE JOGO
 function montarCena(nomeCena) {
-    let i = 0;
+    // Limpa a cena
+    limparCena();
 
     // Request do arquivo JSON
     fetch('../cenas/' + nomeCena + '.json')
@@ -25,11 +26,25 @@ function montarCena(nomeCena) {
             telaContainer.style.backgroundImage = background;
 
             // Colocando botão de retorno no topo de cada lista
-            let voltar = document.createElement("LI");
-            voltar.innerHTML = "VOLTAR";
-            voltar.addEventListener("click", function() {mostrarItem(".menu-items")});
+            let voltar1 = document.createElement("LI");
+            let hr1 = document.createElement("HR");
+            voltar1.innerHTML = "VOLTAR";
+            voltar1.addEventListener("click", () => {mostrarItem(".menu-items")});
+
+            // TODO: Código redundante. Procurar uma maneira de 'replicar' click listener dinamicamente para varios elementos (um for deve bastar)
+            let voltar2 = document.createElement("LI");
+            let hr2 = document.createElement("HR");
+            voltar2.innerHTML = "VOLTAR";
+            voltar2.addEventListener("click", () => {mostrarItem(".menu-items")});
+
+            movimentoContainer.appendChild(voltar1);
+            movimentoContainer.appendChild(hr1);
+
+            examinarContainer.appendChild(voltar2);
+            examinarContainer.appendChild(hr2);
 
             // Criando e concatenando elementos li a lista
+            let i = 0;
             for (let local of locais) {
                 let li = document.createElement("LI");
                 li.id = "local" + i;
@@ -53,9 +68,17 @@ function montarCena(nomeCena) {
         })
         .catch(err => {
             console.log("Request Inválido. Checar url do arquivo.")
-        })
-    
-    
+        })    
+}
+
+function limparCena() {
+    // TODO: muita repetição aqui. procurar unificar seletores e acesso a innerhtml
+    let movimentoAntigo = document.querySelector(".movimento-items");
+    let examinarAntigo = document.querySelector(".examinar-items");
+    let textAntigo = document.querySelector(".text-conteudo");
+    movimentoAntigo.innerHTML = "";
+    examinarAntigo.innerHTML = "";
+    textAntigo.innerHTML = "";
 }
 
 // FUNÇÕES PARA CONTROLE DA INTERFACE
