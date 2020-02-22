ca6c9fe8d63f889829dfb731f39adc6a0094e924
MoisesDuarte <moisesduartejunior@gmail.com>
2020-02-22 13:04:47 -0300

    Adicionando funções para bloqueio e desbloqueio de menu + melhorando menu debug

--- js/ui_functions.js
+++ js/ui_functions.js
@@ -104,23 +104,26 @@ function mostrarDescricao(txt) {
 }
 
 function mostrarFala(arrayFalas) {
-    // TODO: 'Pausar' enquanto a fala estiver correndo pela tela
     let i = 0;
     let textContainer = document.querySelector(".text-container");
     let textConteudo = document.querySelector(".text-conteudo");
     let textNext = document.querySelector(".text-next");
     let falas = arrayFalas;
 
-    // Mostrando Texto Inicial, junto de aviso para clicar
+    // Primeira Fala
     textConteudo.innerHTML = falas[0];
     textNext.style.display = "block";
     i++;
 
+    bloquearMenu();
+
+    // Falas Subsequentes
     textContainer.addEventListener("click", function avancarTexto(){
         if (i == falas.length) {
             textConteudo.innerHTML = "";
             textNext.style.display = "none";
             textContainer.removeEventListener("click", avancarTexto);
+            permitirMenu();
         }
 
         while (i < falas.length) {
@@ -130,4 +133,20 @@ function mostrarFala(arrayFalas) {
             break;
         }
     });
+}
+
+function bloquearMenu() {
+    let elMenus = document.querySelectorAll("li");
+    for (let menu of elMenus) {
+        menu.style.color = 'gray';
+        menu.style.pointerEvents = "none";
+    }
+}
+
+function permitirMenu() {
+    let elMenus = document.querySelectorAll("li");
+    for (let menu of elMenus) {
+        menu.style.color = 'var(--main-primary-color)';
+        menu.style.pointerEvents = "auto";
+    }
 }
