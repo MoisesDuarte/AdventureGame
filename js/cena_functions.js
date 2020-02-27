05167669aacce6c4367c03d25f8e2fd3f15f0afc
MoisesDuarte <moisesduartejunior@gmail.com>
2020-02-27 09:02:50 -0300

    'Limpando' código

--- js/cena_functions.js
+++ js/cena_functions.js
@@ -1,93 +1,80 @@
-// (limparCena, mostrarItem, bloquearMenu - ui_functions.js)
-// FUNÇÕES PARA CONSTRUÇÃO DE CENA DE JOGO
 function montarCena(nomeCena) {
-    // Limpa a cena
     limparCena();
 
-    // TODO: Transformar esse bloco de em uma função, para utilizar em funções de atualização, etc
     let jsonStorage = localStorage.getItem('cenas');
     let cenaJson = JSON.parse(jsonStorage);
     let idCena;
 
-    // Procurando a cena com base em nome passado
     for (let i = 0; i < cenaJson.length; i++) {
         if (cenaJson[i].nome == nomeCena) {
             idCena = i;
         }
     }
 
-    // Definições de Cena
-    let background = "url('img/" + cenaJson[idCena].fundo + "')";
-    let locais = cenaJson[idCena].locais;
-    let pontosInteresse = cenaJson[idCena].pontosInteresse;
-    let atores = cenaJson[idCena].atores;
+    let background = 'url("img/' + cenaJson[idCena].fundo + '")';
+    let arrayLocais = cenaJson[idCena].locais;
+    let arrayPontos = cenaJson[idCena].pontosInteresse;
+    let arrayAtores = cenaJson[idCena].atores;
 
-    // Elementos da Interface
-    let telaContainer = document.querySelector(".tela-container");
-    let movimentoContainer = document.querySelector(".movimento-items");
-    let examinarContainer = document.querySelector(".examinar-items");
-    let falarContainer = document.querySelector(".falar-items");
-    let inventarioContainer = document.querySelector(".inventario-items");
+    let cTela = document.querySelector('.tela-container');
+    let cMovimento = document.querySelector('.movimento-items');
+    let cExaminar = document.querySelector('.examinar-items');
+    let cFalar = document.querySelector('.falar-items');
 
-    // Mostrando plano de fundo da cena
-    telaContainer.style.backgroundImage = background;
+    cTela.style.backgroundImage = background;
 
-    // Inserindo botão VOLTAR fixo no topo de cada menu
-    let elementosLimpos = document.querySelectorAll("ul:not(.menu-items)");        
-    
+    let elementosLimpos = document.querySelectorAll('ul:not(.menu-items)');   
     for (let elemento of elementosLimpos) {
-        let voltar = document.createElement("LI");
-        let hr = document.createElement("HR");
-        voltar.innerHTML = "VOLTAR";
-        voltar.addEventListener("click", () => {mostrarItem(".menu-items")});
+        let voltar = document.createElement('li');
+        let hr = document.createElement('hr');
+        voltar.innerHTML = 'VOLTAR';
+        voltar.addEventListener('click', () => {mostrarItem('.menu-items')});
         elemento.appendChild(voltar);
         elemento.appendChild(hr);
-    }; 
+    }
 
-    // Inserindo locais
     let i = 0;
-    for (let local of locais) {
-        let li = document.createElement("LI");
-        li.id = "local_" + local.id;
+    for (let local of arrayLocais) {
+        let li = document.createElement('li');
+        li.id = 'local_' + local.id;
         li.innerHTML = local.nome;
-        li.addEventListener("click", function() {montarCena(local.nome)});
-        movimentoContainer.appendChild(li);
-        i++;
-    };
-
-    // Inserindo Atores
-    i = 0;
-    for (let ator of atores) {
-        let li = document.createElement("LI");
-        li.id = 'ator_' + ator.id;
-        li.innerHTML = ator.nome;
-        li.addEventListener("click", function() {mostrarTexto(ator.falas[ator.estado])});
-        falarContainer.appendChild(li);
+        li.addEventListener('click', function() {montarCena(local.nome)});
+        cMovimento.appendChild(li);
         i++;
     }
 
-    // Inserindo Pontos de Interesse
     i = 0;
-    for (let ponto of pontosInteresse) {
-        let li = document.createElement("LI");
-        li.id = "ponto" + ponto.id;
+    for (let ponto of arrayPontos) {
+        let li = document.createElement('li');
+        li.id = 'ponto_' + ponto.id;
         li.innerHTML = ponto.nome;
-        li.addEventListener("click", function() {mostrarTexto(ponto.descricao)});
-        examinarContainer.appendChild(li);
+        li.addEventListener('click', function() {mostrarTexto(ponto.descricao)});
+        cExaminar.appendChild(li);
         i++;
-    }   
-    
-}
+    }
+
+    i = 0;
+    for (let ator of arrayAtores) {
+        let li = document.createElement('li');
+        li.id = 'ator_' + ator.id;
+        li.innerHTML = ator.nome;
+        li.addEventListener('click', function() {mostrarTexto(ator.falas[ator.estado])});
+        cFalar.appendChild(li);
+        i++;
+    }    
+};
+
 
 function limparCena() {
-    let elementosAntigos = document.querySelectorAll("ul:not(.menu-items)");
-    let textoAntigo = document.querySelector(".text-conteudo");
+    let elementosAntigos = document.querySelectorAll('ul:not(.menu-items)');
+    let textoAntigo = document.querySelector('.text-conteudo');
     for (let elemento of elementosAntigos) {
         elemento.innerHTML = "";
     }
 
     textoAntigo.innerHTML = "";
-}
+};
+
 
 function guardarCenasInicial() {
     fetch('./cenas/_cenas.json')
@@ -101,7 +88,8 @@ function guardarCenasInicial() {
         localStorage.setItem('cenas', stringJson);
         location.reload();
     })
-}
+};
+
 
 function trocarEstadoAtor(idCena, nomeAtor, estadoNovo) {
     let jsonStorage = localStorage.getItem('cenas');
--- js/ui_functions.js
+++ js/ui_functions.js
@@ -1,59 +1,59 @@
-// FUNÇÕES PARA CONTROLE DA INTERFACE
 function mostrarItem(item) {
-    let el = document.body.querySelectorAll("ul");
+    let el = document.body.querySelectorAll('ul');
     let newEl = document.body.querySelector(item);
 
     el.forEach(element => {
-        element.style.display = "none";
+        element.style.display = 'none';
     });
 
-    newEl.style.display = "block";
-}
+    newEl.style.display = 'block';
+};
+
 
 function mostrarTexto(arrayFalas) {
     let i = 0;
-    let textContainer = document.querySelector(".text-container");
-    let textConteudo = document.querySelector(".text-conteudo");
-    let textNext = document.querySelector(".text-next");
-    let falas = arrayFalas;
+    let textContainer = document.querySelector('.text-container');
+    let textConteudo = document.querySelector('.text-conteudo');
+    let textNext = document.querySelector('.text-next');
+    let atorFalas = arrayFalas;
 
-    // Primeira Fala
-    textConteudo.innerHTML = falas[0];
-    textNext.style.display = "block";
+    textConteudo.innerHTML = atorFalas[0];
+    textNext.style.display = 'block';
     i++;
 
     bloquearMenu();
 
-    // Falas Subsequentes
-    textContainer.addEventListener("click", function avancarTexto(){
-        if (i == falas.length) {
-            textConteudo.innerHTML = "";
-            textNext.style.display = "none";
-            textContainer.removeEventListener("click", avancarTexto);
+    textContainer.addEventListener('click', function avancarTexto(){
+        if (i == atorFalas.length) {
+            textConteudo.innerHTML = '';
+            textNext.style.display = 'none';
+            textContainer.removeEventListener('click', avancarTexto);
             permitirMenu();
         }
 
-        while (i < falas.length) {
-            console.log(i + " : " + falas[i]);
-            textConteudo.innerHTML = falas[i];
+        while (i < atorFalas.length) {
+            console.log(i + ' : ' + atorFalas[i]);
+            textConteudo.innerHTML = atorFalas[i];
             i++;
             break;
         }
     });
-}
+};
+
 
 function bloquearMenu() {
-    let elMenus = document.querySelectorAll("li");
+    let elMenus = document.querySelectorAll('li');
     for (let menu of elMenus) {
         menu.style.color = 'gray';
-        menu.style.pointerEvents = "none";
+        menu.style.pointerEvents = 'none';
     }
-}
+};
+
 
 function permitirMenu() {
-    let elMenus = document.querySelectorAll("li");
+    let elMenus = document.querySelectorAll('li');
     for (let menu of elMenus) {
         menu.style.color = 'var(--main-primary-color)';
-        menu.style.pointerEvents = "auto";
+        menu.style.pointerEvents = 'auto';
     }
-}
+};
