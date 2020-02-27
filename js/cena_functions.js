91ed36419f96a6bc12a59ff695c7837c4b171176
MoisesDuarte <moisesduartejunior@gmail.com>
2020-02-27 15:35:57 -0300

    Comentando código

--- js/cena_functions.js
+++ js/cena_functions.js
@@ -1,24 +1,29 @@
+// FUNÇÕES PARA MANUTENÇÃO DE CENA
 function montarCena(nomeCena) {
     limparCena();
 
+    // Lendo arquivos JSON e guardando em objetos em localStorage
     let jsonCena = localStorage.getItem('cenas');
     let jsonInv = localStorage.getItem('inventario');
     let cenaJson = JSON.parse(jsonCena);
     let invJson = JSON.parse(jsonInv);
-    let idCena;
 
+    // Procurando pelo id da cena com base em nome (passar diretamente o id seria melhor)
+    let idCena;
     for (let i = 0; i < cenaJson.length; i++) {
         if (cenaJson[i].nome == nomeCena) {
             idCena = i;
         }
     }
 
+    // Guardando objetos da cena em arrays
     let background = 'url("img/' + cenaJson[idCena].fundo + '")';
     let arrayLocais = cenaJson[idCena].locais;
     let arrayPontos = cenaJson[idCena].pontosInteresse;
     let arrayAtores = cenaJson[idCena].atores;
     let arrayInventario = invJson;
 
+    // Selecionando container dos elementos da tela
     let cTela = document.querySelector('.tela-container');
     let cMovimento = document.querySelector('.movimento-items');
     let cExaminar = document.querySelector('.examinar-items');
@@ -26,8 +31,10 @@ function montarCena(nomeCena) {
     let cInventario = document.querySelector('.inventario-items');
     let cInteracoes = document.querySelector('.inventario-interacoes');
 
+    // Definindo imagem de fundo da cena
     cTela.style.backgroundImage = background;
 
+    // Adicionando a opção voltar e um divisor no topo de cada lista
     let elementosLimpos = document.querySelectorAll('ul:not(.menu-items)');   
     for (let elemento of elementosLimpos) {
         let voltar = document.createElement('li');
@@ -40,7 +47,7 @@ function montarCena(nomeCena) {
         elemento.appendChild(hr);
     }
 
-    // Menus
+    // Adicionando opções a cada menu
     for (let local of arrayLocais) {
         let li = document.createElement('li');
         li.id = 'local_' + local.id;
@@ -73,7 +80,7 @@ function montarCena(nomeCena) {
         cInventario.appendChild(li);
     }
 
-    // Interações
+    // Adicionando opções ao menu de interações
     let intTitle = document.createElement('p');
     intTitle.innerHTML = 'USAR EM:';
     cInteracoes.appendChild(intTitle);
@@ -97,17 +104,22 @@ function montarCena(nomeCena) {
 
 
 function limparCena() {
+    // Selecionando por elementos com conteudo 'antigo'
     let elementosAntigos = document.querySelectorAll('ul:not(.menu-items)');
     let textoAntigo = document.querySelector('.text-conteudo');
+
+    // Limpando cada elemento
     for (let elemento of elementosAntigos) {
         elemento.innerHTML = "";
     }
 
+    // Limpando caixa de texto (Quem sabe poderia ser em outro lugar, como mostrarItem)
     textoAntigo.innerHTML = "";
 };
 
 
 function guardarEstadoInicial() {
+    // Fetch de arquvos locais json para salvar em localStorage
     fetch('../cenas/cenas.json')
         .then(res => { return res.json() })
         .then(data => {
@@ -130,15 +142,18 @@ function checarInteracao(idCena, nome, objTriggers, tipo) {
     let cInteracoes = document.querySelector('.inventario-interacoes');
     let itemAtivo = cInteracoes.dataset.itemAtivo;
 
+    // Procurando por atributo itemTrigger com nome igual ao itemAtivo
     for (let trigger of objTriggers) {
         if (trigger.itemTrigger == itemAtivo) {
             console.log("Interação Válida");
+            // Checando se a interação é com ator ou ponto
             if (tipo == 'ator') {
                 trocarEstadoAtor(idCena, nome, trigger.idEstado);
             } else if (tipo == 'ponto') {
                 console.log("Interação com ponto");
             }
         } else {
+            // Mensagem caso não for possível utilizar o item com objeto
             cText.innerHTML = 'Você utiliza <i>' + itemAtivo + '</i> em <i>' + nome + '</i> mas nada acontece.';
         }
     }
@@ -149,6 +164,7 @@ function trocarEstadoAtor(idCena, nomeAtor, estadoNovo) {
     let jsonStorage = localStorage.getItem('cenas');
     let cenaJson = JSON.parse(jsonStorage);
 
+    // Procura por ator com base em nome e troca seu estado pelo novo
     for (let ator of cenaJson[idCena].atores) {
         if (ator.nome == nomeAtor) {
             ator.estado = estadoNovo;
@@ -157,7 +173,10 @@ function trocarEstadoAtor(idCena, nomeAtor, estadoNovo) {
         }
     }   
     
+    // Salvando cenas atualizadas em localStorage 
     localStorage.setItem('cenas', JSON.stringify(cenaJson));
+
+    // TODO: Apagar item do inventário após uso
 
     montarCena(cenaJson[idCena].nome);
 }
